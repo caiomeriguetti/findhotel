fbbcfb80bb20c542bb31f4f3423487a6f83eef49
caiomeriguetti <caiomeriguetti@gmail.com>
2018-10-08 23:51:51 -0300

    removed useless code

--- rest-api/code/geolocationapi/app.py
+++ rest-api/code/geolocationapi/app.py
@@ -2,14 +2,13 @@
 import falcon
 
 from geolocationapi.routes import main_routes
-from falcon_extensions import Request
 
 
 class GeolocationApi(falcon.API):
 
     def __init__(self, *args, **kwargs):
 
-        super(GeolocationApi, self).__init__(request_type=Request, *args, **kwargs)
+        super(GeolocationApi, self).__init__(*args, **kwargs)
 
         for route in main_routes:
             self.add_route(route[0], route[1])
